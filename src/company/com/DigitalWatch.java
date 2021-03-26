66cea9bb78b3187c60648822ca2d464d69a53ee8
Evgeniy-Bear <78522946+Evgeniy-Bear@users.noreply.github.com>
2021-03-26 22:20:11 +0500

    dz3

---
+++ src/company/com/DigitalWatch.java
@@ -0,0 +1,10 @@
+package company.com;
+
+public class DigitalWatch implements Watch{
+
+
+    @Override
+    public void showTime() {
+        System.out.println("DigitalWatch");
+    }
+}
---
+++ src/company/com/DigitalWatchMaker.java
@@ -0,0 +1,8 @@
+package company.com;
+
+public class DigitalWatchMaker implements WatchMaker{
+    @Override
+    public Watch createWatch() {
+        return new DigitalWatch();
+    }
+}
